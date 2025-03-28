ec9238797e997f178ec3a345572b4dfaffb90030
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-03-28 16:43:11 +0530

    updated navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,75 @@
 import React from "react";
 import Logo from "../../assets/website/logo.png";
+import { FaCaretDown } from "react-icons/fa";
+
+const Menu = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Best Seller",
+    link: "/#services",
+  },
+];
+
+const DropdownLinks = [
+  {
+    name: "Trending Books",
+    link: "/#",
+  },
+  {
+    name: "Best Selling",
+    link: "/#",
+  },
+  {
+    name: "Authors",
+    link: "/#",
+  },
+];
 
 const Navbar = () => {
   return (
-    <div>
-      <div className="container">
+    <div className="shadow-lg">
+      <div className="container py-3 sm:py-0">
         <div className="flex justify-between items-center">
           <div>
-            <a href="#" className="fond-bold text-3xl sm:text-3xl flex gap-2">
+            <a href="#" className="font-bold text-3xl sm:text-3xl flex gap-2">
               <img src={Logo} alt="" className="w-10" />
               Books
             </a>
           </div>
           <div>
-            <ul>
-              <li>Home</li>
+            <ul className="items-center gap-4 hidden sm:flex">
+              {Menu.map((menu) => (
+                <li>
+                  <a
+                    href={menu.link}
+                    className="inline-block py-4 px-4 hover:text-primary duration-200"
+                  >
+                    {menu.name}
+                  </a>
+                </li>
+              ))}
+              <li className="group relative cursor-pointer">
+                <a href="/#" className="flex h-[72px] items-center gap[2px]">
+                  Quick Links
+                  <span>
+                    <FaCaretDown className="transition duration-300 group-hover:rotate-180" />
+                  </span>
+                </a>
+              </li>
+              <div>
+                <ul>
+                  {DropdownLinks.map((data) => (
+                    <li>
+                      <a href={data.link}>{data.name}</a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </ul>
           </div>
         </div>
