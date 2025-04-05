8e67923376692c4888ae8066ff53a16de4b6b033
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-04-05 19:59:31 +0530

    inialize the BestBook.jsx

--- src/Components/BestBook/BestBook.jsx
+++ src/Components/BestBook/BestBook.jsx
@@ -4,7 +4,17 @@ const BestBook = () => {
   return (
     <>
       <div>
-        <div className="container"></div>
+        <div className="container">
+          <div className="text-center mb-20 max-w-[400px] mx-auto">
+            <p>Trending Books</p>
+            <h1>Best Books</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
+              eligendi ad fugiat aut saepe labore, libero modi fuga alias
+              obcaecati{" "}
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
