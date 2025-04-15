f97c14a4c775f280163e8e0fdefbab4b8df41549
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-04-15 12:08:04 +0530

    updated the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Navbar from "./Components/Navbar/Navbar";
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Hero />
-      <BestBook />
-    </div>
+    <>
+      <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
+        <Navbar />
+        <Hero />
+        <BestBook />
+      </div>
+    </>
   );
 };
 
