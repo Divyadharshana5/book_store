6f763038f9f53de5141f7c50173c5df7d391c132
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-04-17 11:36:51 +0530

    update AllBooks

--- src/Components/AllBooks/AllBooks.jsx
+++ src/Components/AllBooks/AllBooks.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const AllBooks = () => {
-  return <div>AllBooks</div>;
+  return (
+    <>
+      <div>
+        <div className="container">
+          <div className="text-center mb-20 max-w-[400px] mx-auto">
+            <p className="text-sm font-bold bg-clip-text bg-gradient-to-r text-blue-400">
+              Top Books for You!!
+            </p>
+            <h1 className="text-3xl font-bold">Top Books</h1>
+            <p className="text-xs text-gray-400">
+              There is a wisdom that is woe; but there is a woe that is
+              madness...
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AllBooks;
