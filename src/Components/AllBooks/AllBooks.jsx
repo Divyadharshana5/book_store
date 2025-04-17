ccc1bdf2ce2ffd175da1afa0a5aeae3dfd8bae5c
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-04-17 12:20:33 +0530

    finished AllBooks

--- src/Components/AllBooks/AllBooks.jsx
+++ src/Components/AllBooks/AllBooks.jsx
@@ -91,7 +91,7 @@ const AllBooks = () => {
             </p>
           </div>
           <div>
-            <div>
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 place-items-center gap-5">
               {BooksData.map((data) => (
                 <div>
                   <img
