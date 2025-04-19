8ea0f3f42c750fbc0c847d743e8fe9df667b39f2
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-04-19 19:43:12 +0530

    update Banner.jsx

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -15,6 +15,7 @@ const Banner = () => {
           >
             <div>
               <img
+                data-aos="zoom-in"
                 src={LibraryImg}
                 alt=""
                 className="max-w-[400px] block mx-auto h-[350px] w-full drop-shadow-[-10px_10px_12px_rgba(0,0,0,1)] 
@@ -22,7 +23,10 @@ const Banner = () => {
               />
             </div>
 
-            <div className="flex flex-col justify-center gap-6 sm:pt-0">
+            <div
+              data-aos="slide-up"
+              className="flex flex-col justify-center gap-6 sm:pt-0"
+            >
               <h1 className="text-3xl sm:text-4xl font-bold">
                 Library at Your Fingertips!
               </h1>
