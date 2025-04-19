761bd3f3697d628d3e78c6d4e39dd895a195f934
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-04-19 20:24:18 +0530

    Inialized Popup.jsx

--- src/Components/Popup/Popup.jsx
+++ src/Components/Popup/Popup.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { IoCloseOutline } from "react-icons/io5";
 
-const Popup = () => {
-  return <div>Popup</div>;
+const Popup = ({ orderPopup, handleOrderPopup }) => {
+  return (
+    <>
+      {orderPopup && (
+        <div className="h-screen w-screen fixed top-0 left-0 z-20 backdrop-blur-sm">
+          <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white dark:bg-gray-900 dark:text-white duration-200 p-4 rounded-md shadow-md w-[300px]">
+            <div className="flex items-center justify-center">
+              <h1>Order Your Book</h1>
+              <div>
+                <IoCloseOutline
+                  className="text-2xl cursor-pointer"
+                  onClick={() => handleOrderPopup(false)}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Popup;
